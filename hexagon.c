190789b9465fd1fdef9388a346b413f091fe0b2a
DELASIWO <129696928+Delasiwo@users.noreply.github.com>
2023-05-30 07:04:09 -0700

    Update hexagon.c

--- hexagon.c
+++ hexagon.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
-* @c: Char to be evaluated.
+ * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -20,7 +20,7 @@ int is_printable(char c)
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
-*/
+ */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
@@ -39,7 +39,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 /**
  * is_digit - Verifies if a char is a digit
-* @c: Char to be evaluated
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -57,7 +57,7 @@ int is_digit(char c)
  * @size: Number indicating the type to be casted.
  *
  * Return: Casted value of num
-*/
+ */
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
